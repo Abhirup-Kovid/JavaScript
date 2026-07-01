e9af8b5efc586a7e014c03bbd1ea0e5be0f97798
Abhirup Kovid <abhirupkovid@gmail.com>
2026-07-01 07:53:34 +0000

    Variables and Constants

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "Abhi@gmail.com"
+var accountPassword = "12345"
+accountCity = "BBSR"
+let accountState;
+
+// accountId = 2 //Not allowed
+accountEmail ="ABHI@gmail.com"
+accountCity= "Bengaluru"
+accountPassword = "212121"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
