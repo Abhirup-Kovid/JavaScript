ddd3809ae64613f3336c977899412f6d3a75674a
Abhirup Kovid <abhirupkovid@gmail.com>
2026-07-01 08:18:54 +0000

    DataTypes

---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3) // It will show error as we are using nodeJs now browser if using browser there will be a pop up as 6
+
+// console.log(3+3); console.log("Abhi"); //Readibility Checkout
+
+let name="Abhirup" //String
+let age=21 //Int
+let isLoggedIn=false //Boolean
+let state=null
+
+//PRIMITIVE DATATYPES
+// number => 2^53
+// bigint => large number
+// string => " "
+// boolean => true/false
+// null => Standalone value ,representation of empty value also can be assigned
+// undefined => When value not defined, cannot be assigned
+// symbol => Finding the uniqueness in REACT
+
+// object
+
+// console.log(typeof age);
+// console.log(typeof "Abhi");
+// console.log(typeof null);
+// console.log(typeof undefined);
+
