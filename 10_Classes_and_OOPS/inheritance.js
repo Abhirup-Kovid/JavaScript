90090be9f9d53cce6571de25a06db100e234ec6b
Abhirup Kovid <abhirupkovid@gmail.com>
2026-09-02 14:51:53 +0000

    correted the typo

--- 10_Classes_and_OOPS/inheritance.js
+++ 10_Classes_and_OOPS/inheritance.js
@@ -10,7 +10,7 @@ class User {
 
 class Teacher extends User{
     constructor (username, email, password){
-        super{username}
+        super(username)
         this.email= email
         this.password=password
     }
